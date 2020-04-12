3f77e161f0304d1fe0c1b926b9922e17fbf9fa01
keitoku.ikegami <keitoku.ikegami2020@klas.ch>
2020-04-12 19:09:46 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by IKEGAMIKeitoku on 12.04.20.
+//  Copyright © 2020 keitoku.ikegami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
