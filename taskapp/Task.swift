0c7d1af7c63d6fba6525d4a2f9327cde09958dae
keitoku.ikegami <keitoku.ikegami2020@klas.ch>
2020-04-17 16:07:37 +0900

    完成

---
+++ taskapp/Task.swift
@@ -0,0 +1,26 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by IKEGAMIKeitoku on 12.04.20.
+//  Copyright © 2020 keitoku.ikegami. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+
+    @objc dynamic var id = 0
+
+    @objc dynamic var title = ""
+
+    @objc dynamic var contents = ""
+    
+    @objc dynamic var genre = ""
+
+    @objc dynamic var date = Date()
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
